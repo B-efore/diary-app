06aa02dd24f0f4c9df8c9f7ae575638afbb741c5
jiwon <ljw010512@naver.com>
2022-01-07 16:15:30 +0900

    프로젝트 시작

---
+++ app/src/main/java/com/example/diaryapplication/EditCheckListActivity.java
@@ -0,0 +1,14 @@
+package com.example.diaryapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditCheckListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_check_list);
+    }
+}
