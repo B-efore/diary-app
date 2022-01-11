cc7879a06a600eeb8bbf7b2588612dc4ad8bdd4a
Hana <yhn8888@naver.com>
2022-01-12 02:29:37 +0900

    타임라인 어뎁터, 타임라인 관리 파일 생성

--- app/src/main/java/com/example/diaryapplication/MainActivity.java
+++ app/src/main/java/com/example/diaryapplication/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.diaryapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    
+    //타임라인 리스트뷰 변수 선언
+    private ListView timelineListView;
+    //타임라인 어뎁터 선언
+    private TimelineAdapter timelineAdapter;
+    //타임라인 데이터를 담는 리스트 선언
+    private List<TimelineData> timelineList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //타임라인 리스트뷰 초기화 (아이디 이름은 수정예정)
+        timelineListView = (ListView) findViewById(R.id.timelineListView);
+        //타임라인 리스트 초기화
+        timelineList = new ArrayList<TimelineData>();
+        //어뎁터에 타임라인 리스트 내용 넣어주기
+        timelineAdapter = new TimelineAdapter(getApplicationContext().timelineList);
+        //타임라인 리스트뷰에 어뎁터 연결
+        timelineListView.setAdapter(timelineAdapter);
+
     }
 }
---
+++ app/src/main/java/com/example/diaryapplication/TimelineAdapter.java
@@ -0,0 +1,56 @@
+package com.example.diaryapplication;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TimelineAdapter extends BaseAdapter {
+
+    private Context context;
+    //타임라인 내용을 저장할 리스트 선언
+    private List<TimelineData> timelineList;
+    
+    //생성자 선언
+    public TimelineAdapter(Context context, List<TimelineData> timelineList) {
+        this.context = context;
+        this.timelineList = timelineList;
+    }
+
+    @Override
+    public int getCount() {
+        return timelineList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return timelineList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        //타임라인에 해당하는 view 생성
+        View v = View.inflate(context, R.layout.activity_timeline, null);
+        //시간 텍스트 표시하는 view 객체 생성
+        TextView timeText = (TextView) v.findViewById(R.id.time);
+        //일정내용 텍스트 표시하는 view 객체 생성
+        TextView scheduleText = (TextView) v.findViewById(R.id.schedule);
+
+        //객체값들을 리스트에 저장
+        timeText.setText(timelineList.get(i).getTime());
+        scheduleText.setText(timelineList.get(i).getSchedule());
+
+        //v의 태그(고유 이름) 설정
+        v.setTag(timelineList.get(i).getTime());
+        return v;
+
+    }
+}
