65232613678d4035109e5278b7b7d5c215da113e
jiwon <ljw010512@naver.com>
2022-01-24 14:43:04 +0900

    각종 기능 보완, 수정

--- app/src/main/java/com/example/diaryapplication/MainCalendar.java
+++ app/src/main/java/com/example/diaryapplication/MainCalendar.java
@@ -1,14 +1,99 @@
 package com.example.diaryapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainCalendar extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainCalendar extends AppCompatActivity implements View.OnClickListener{
+
+    CalendarView calendarView;
+    Calendar calendar;
+    Date date;
+    ImageButton accountBtn, swapBtn, marketBtn;
+
+    String userDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_calendar);
+
+
+        calendarView = findViewById(R.id.calendarView);
+        accountBtn = findViewById(R.id.accountIcon);
+        swapBtn = findViewById(R.id.swapIcon);
+        marketBtn = findViewById(R.id.marketIcon);
+
+        calendar = Calendar.getInstance();
+        date = new Date(calendarView.getDate());
+        calendar.setTime(date);
+
+        userDate = Integer.toString(calendar.get(Calendar.YEAR)) + "-"
+                + Integer.toString(calendar.get(Calendar.MONTH)) + "-"
+                + Integer.toString(calendar.get(Calendar.DATE));
+
+        Log.d("Date", "현재 날짜: " + userDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth){
+
+                if(userDate.equals(year + "-" + month + "-" + dayOfMonth)){
+                    startActivity(new Intent(getApplicationContext(), DetailCalendar.class).putExtra("Date", date));
+                }
+                else {
+                    userDate = Integer.toString(year) + "-"
+                            + Integer.toString(month) + "-"
+                            + Integer.toString(dayOfMonth);
+                }
+
+                Log.d("Date", "현재 날짜: " + userDate);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Log.d("Date", "현재 날짜: " + userDate);
+
+        switch (view.getId()) {
+            case R.id.accountIcon:
+                startActivity(new Intent(this, Setting.class));
+                break;
+            case R.id.marketIcon:
+                CustomDialog customDialog = new CustomDialog(MainCalendar.this);
+                customDialog.callFunction("준비중입니다!");
+                break;
+            case R.id.swapIcon:
+                startActivity(new Intent(this, DetailCalendar.class).putExtra("Date", userDate));
+                break;
+//            case 편집 버튼:
+//                date = new Date(calendarView.getDate());
+//                calendar.setTime(date);
+//
+//                String date = Integer.toString(calendar.get(Calendar.YEAR)) + "-"
+//                        + Integer.toString(calendar.get(Calendar.MONTH)) + "-"
+//                        + Integer.toString(calendar.get(Calendar.DATE));
+//
+//                startActivity(new Intent(this, EditCheckListActivity.class).putExtra("Date", userDate));
+//                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        //super.onBackPressed();
+        CustomDialog customDialog = new CustomDialog(MainCalendar.this);
+        customDialog.callFunction("다이어리 작성을 끝내겠습니까?");
     }
 }
---
+++ app/src/main/java/com/example/diaryapplication/SettingInfo.java
@@ -0,0 +1,125 @@
+package com.example.diaryapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.example.diaryapplication.database.UserDB;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SettingInfo extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+    private Button registerBtn;
+    private EditText userName, userMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_info);
+
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.hide();
+
+        //파이어베이스 데이터베이스 사용
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        registerBtn = findViewById(R.id.registerBtn);
+        userName = findViewById(R.id.userName);
+        userMessage = findViewById(R.id.userMessage);
+
+        registerBtn.setEnabled(false);
+
+        userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (userName.length() > 0 && userMessage.length() > 0) {
+                    registerBtn.setEnabled(true);
+                    registerBtn.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+                    registerBtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.round_button_ok));
+                } else {
+                    registerBtn.setEnabled(false);
+                    registerBtn.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.deep_gray));
+                    registerBtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.round_button));
+                }
+            }
+        });
+
+        userMessage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (userName.length() > 0 && userMessage.length() > 0) {
+                    registerBtn.setEnabled(true);
+                    registerBtn.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+                    registerBtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.round_button_ok));
+                } else {
+                    registerBtn.setEnabled(false);
+                    registerBtn.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.deep_gray));
+                    registerBtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.round_button));
+                }
+            }
+        });
+
+        //확인 버튼, 유저 DB 업데이트!
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String strName = userName.getText().toString();
+                String strMessage = userMessage.getText().toString();
+
+                FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                UserDB mUser = new UserDB();
+
+                mUser.set_id(firebaseUser.getUid());
+                mUser.setName(strName);
+                mUser.setMessage(strMessage);
+
+                mDatabase.child("users").child(firebaseUser.getUid()).child("info").setValue(mUser);
+
+                Intent registerIntent = new Intent(SettingInfo.this, Setting.class);
+                startActivity(registerIntent);
+                finish();
+            }
+        });
+    }
+
+    // DB에 유저정보 업데이트
+//    public void writeNewUser(String userId, String name, String message) {
+//        UserDB user = new UserDB(name, message);
+//
+//        mDatabase.child("users").child(userId).setValue(user);
+//    }
+}
